53786bbf5d4752e4f4a345ce51e048749a741c0b
Andre Sun <105667299+and2049@users.noreply.github.com>
2025-07-07 13:08:11 +0800

    Function prototypes for sigmoid and ReLu activation functions

--- library.h
+++ library.h
@@ -27,6 +27,7 @@ typedef struct {
     double learning_rate;
 } Network;
 
+// Matrix functions
 
 /**
 * @brief Creates and allocates memory for new matrix
@@ -49,5 +50,96 @@ void matrix_free(Matrix* m);
 */
 void matrix_view(const Matrix* m);
 
+/**
+ * @brief Fills matrix with random values [-1.0,1.0]
+ * @param m Matrix pointer
+*/
+void matrix_randomize(Matrix* m);
+
+/**
+* @brief Matrix multiplication
+* @param a First matrix
+* @param b Second matrix
+* @return product of two matrices
+*/
+Matrix* matrix_multiply(const Matrix* a, const Matrix* b);
+
+/**
+* @brief Matrix addition
+* @param a First matrix
+* @param b Second matrix
+* @return sum of two matrices
+*/
+Matrix* matrix_add(const Matrix* a, const Matrix* b);
+
+/**
+* @brief Matrix transpose, interchanging rows and columns of original matrix
+* @param m Matrix
+* @return transpose of matrix m
+*/
+Matrix* matrix_transpose(const Matrix* m);
+
+/**
+* @brief Applies a function to each element of a matrix
+* @param m Matrix
+* @param func Activation function to apply to matrix element
+*/
+void matrix_map(Matrix* m, double (*func) (double));
+
+// Activation functions
+
+/**
+* @brief Sigmoid activation function
+* @param x input value
+* @return sigmoid value of x
+*/
+double sigmoid(double x);
+
+/**
+* @brief Derivative of sigmoid function
+* @param x input value
+* @return derivative of sigmoid at original value
+*/
+double sigmoid_derivative(double x);
+
+/**
+* @brief Applies sigmoid function to each element of a matrix
+* @param m Matrix
+*/
+void activation_sigmoid(Matrix* m);
+
+/**
+* @brief Computes derivative of sigmoid function and applies value to derivative matrix
+* @param activated Matrix following sigmoid activation
+* @param derivative Output matrix storing derivatives
+*/
+void derivative_sigmoid(Matrix* activated, Matrix* derivative);
+
+/**
+* @brief Rectified linear unit activation
+* @param x input value
+* @return ReLu of x
+*/
+double relu(double x);
+
+/**
+ * @brief Derivative of ReLu function
+ * @param x input value
+ * @return dertivative of ReLu at x
+*/
+double relu_derivative(double x);
+
+/**
+* @brief Applies ReLu function to elements of a matrix
+* @param m matrix
+*/
+void activation_relu(Matrix* m);
+
+/**
+* @brief Computes derivative
+* @param activated Matrix after activation
+* @param derivative Output matrix to store derivative values
+*/
+void derivative_relu(Matrix* activated, Matrix* derivative);
 
 #endif //NN_C_UDA_LIBRARY_H
